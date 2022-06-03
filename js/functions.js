d74b0e5790a300a43056ea97b77aa249bd726b58
Camilo Vallejo <juancamilovallejo@gmail.com>
2022-06-03 11:53:47 -0500

    Added comentaries

--- js/functions.js
+++ js/functions.js
@@ -5,11 +5,13 @@ function pago (interesPorcentaje, capital, cuotas) {
    return (interesPorcentaje * capital)/ (1-Math.pow((1+interesPorcentaje), -cuotas))
 }
 
-/*  */
+/* Function calculo de pago de capital */
 
 function amortizacion (cuotaMensual, interesesFijos) {
     return (cuotaMensual - interesesFijos)
 }
+
+/* Function para calcular el pago de intereses mensuales */
 
 function intereses (capital, interesPorcentaje) {
     return (capital * interesPorcentaje)
