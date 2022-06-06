93932a2bc2ed3a94a1ae160ef2d381f7879ddb30
Camilo Vallejo <juancamilovallejo@gmail.com>
2022-06-05 20:07:56 -0500

    Added bucle for. In order to show more quotes/months

--- js/desafio.js
+++ js/desafio.js
@@ -1,14 +1,26 @@
 
-
 let capital = parseInt(prompt ("Ingrese el capital a prestar"));
 let cuotas = parseInt(prompt("Ingrese el numero de cuotas"));
 let interesBase = parseFloat(prompt ("¿A que interes le presta el banco?"));
 
 const interesPorcentaje = interesBase/100;
 const interesesFijos = intereses (capital, interesPorcentaje);
-const cuotaMensual = pago (interesPorcentaje, capital, cuotas);
+const cuotaMensual =  pago (interesPorcentaje, capital, cuotas);
 let pagoAmortizacion = amortizacion (cuotaMensual, interesesFijos);
+let saldoFinal = valorFinal (capital,pagoAmortizacion);
 
-alert ("Tu cuota mensual fija es de $" + cuotaMensual);
-alert ("El primer pago que realices será de $" + pagoAmortizacion + " por concepto de capital" );
-alert ("Los intereses que pagaras en tu primera cuota serán de $" + interesesFijos);
+alert ("Tu cuota mensual fija es de $" + cuotaMensual.toFixed(2));
+alert (`Los intereses que se pagaran en tu cuota numero 1 seran de $${interesesFijos.toFixed(2)} y $${pagoAmortizacion.toFixed(2)} por concepto de capital. 
+Y tendrás un saldo final de $${saldoFinal.toFixed(2)}`)
+
+for (let i= 1; i < cuotas; i++) {
+    capital = saldoFinal
+    let interesesMensuales = capital* interesPorcentaje
+    let amortizacionMensual = cuotaMensual - interesesMensuales
+    saldoFinal = capital - amortizacionMensual
+    alert (`Los intereses que se pagaran en tu cuota numero ${i+1} seran de $${interesesMensuales.toFixed(2)} y $${amortizacionMensual.toFixed(2)} por concepto de capital. Y tendrás un saldo final de $${saldoFinal.toFixed(2)}`)
+}
+
+
+
+
--- js/functions.js
+++ js/functions.js
@@ -2,19 +2,24 @@
 /* Function calculo de cuota mensual fija */
 
 function pago (interesPorcentaje, capital, cuotas) {
-   return (interesPorcentaje * capital)/ (1-Math.pow((1+interesPorcentaje), -cuotas))
+   return (interesPorcentaje * capital)/ (1-Math.pow((1+interesPorcentaje), -cuotas));
 }
 
 /* Function calculo de pago de capital */
 
 function amortizacion (cuotaMensual, interesesFijos) {
-    return (cuotaMensual - interesesFijos)
+    return (cuotaMensual - interesesFijos);
 }
 
 /* Function para calcular el pago de intereses mensuales */
 
 function intereses (capital, interesPorcentaje) {
-    return (capital * interesPorcentaje)
+    return (capital * interesPorcentaje);
 }
 
+/* Function para calcular el valor debido al finalizar el ejercicio financiero */
 
+function valorFinal (capital, pagoAmortizacion) {
+    return (capital - pagoAmortizacion);
+}
+     
