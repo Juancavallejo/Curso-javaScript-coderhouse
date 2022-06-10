10cfeec6995207d55115290307af271a548df647
Camilo Vallejo <juancamilovallejo@gmail.com>
2022-06-10 08:16:21 -0500

    Added constructor function, arrays and methods for the arrays

--- js/desafio.js
+++ js/desafio.js
@@ -2,6 +2,8 @@
 let capital = parseInt(prompt ("Ingrese el capital a prestar"));
 let cuotas = parseInt(prompt("Ingrese el numero de cuotas"));
 let interesBase = parseFloat(prompt ("¿A que interes le presta el banco?"));
+/* Array -- Almacenar los intereses, amortización y saldo final de cada cuota */
+const cuotasAlmacenadas = [];
 
 const interesPorcentaje = interesBase/100;
 const interesesFijos = intereses (capital, interesPorcentaje);
@@ -9,18 +11,32 @@ const cuotaMensual =  pago (interesPorcentaje, capital, cuotas);
 let pagoAmortizacion = amortizacion (cuotaMensual, interesesFijos);
 let saldoFinal = valorFinal (capital,pagoAmortizacion);
 
+/* Información relativa a la primera cuota */
 alert ("Tu cuota mensual fija es de $" + cuotaMensual.toFixed(2));
 alert (`Los intereses que se pagaran en tu cuota numero 1 seran de $${interesesFijos.toFixed(2)} y $${pagoAmortizacion.toFixed(2)} por concepto de capital. 
 Y tendrás un saldo final de $${saldoFinal.toFixed(2)}`)
 
+/* Información relativa a la segunda cuota y siguientes */
+
 for (let i= 1; i < cuotas; i++) {
-    capital = saldoFinal
-    let interesesMensuales = capital* interesPorcentaje
-    let amortizacionMensual = cuotaMensual - interesesMensuales
-    saldoFinal = capital - amortizacionMensual
-    alert (`Los intereses que se pagaran en tu cuota numero ${i+1} seran de $${interesesMensuales.toFixed(2)} y $${amortizacionMensual.toFixed(2)} por concepto de capital. Y tendrás un saldo final de $${saldoFinal.toFixed(2)}`)
+    capital = saldoFinal;
+    let interesesMensuales = capital* interesPorcentaje;
+    let amortizacionMensual = cuotaMensual - interesesMensuales;
+    saldoFinal = capital - amortizacionMensual;
+    const cuota1 = new Cuota (interesesMensuales, amortizacionMensual, saldoFinal)
+    cuotasAlmacenadas.push (cuota1)
 }
 
+/* Bucle for para conocer los intereses, amortización y saldo final de cada cuota 
+a partir de la segunda cuota y siguientes */
 
+for (const cuotas of cuotasAlmacenadas) {
+    cuotas.info ()
+}
+
+/* Nuevo array unicamente de saldos finales en cada cuota con el metodo map */
+
+const saldosFinales = cuotasAlmacenadas.map ((el) => el.saldoFinal)
+console.log (saldosFinales)
 
 
--- js/functions.js
+++ js/functions.js
@@ -22,4 +22,13 @@ function intereses (capital, interesPorcentaje) {
 function valorFinal (capital, pagoAmortizacion) {
     return (capital - pagoAmortizacion);
 }
-     
+
+/* Function para ir guardando la información relativa a cada cuota y crear un array de objetos */
+
+function Cuota (interesesMensuales, amortizacionMensual, saldoFinal) {
+    this.interesesMensuales = interesesMensuales.toFixed(2);
+    this.amortizacionMensual = amortizacionMensual.toFixed(2);
+    this.saldoFinal = saldoFinal.toFixed(2)
+    this.info = function () {console.log (`Para esta cuota, se pagará de intereses mensuales $` + this.interesesMensuales, 
+    `por concepto de capital un valor de $` + this.amortizacionMensual, `y un saldo final de $` + this.saldoFinal)}
+}
